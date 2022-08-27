c20e03cf826308ca06ae5093b57c6da5acbd6704
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-08-27 07:52:56 -0400

    added arrays.ts

---
+++ arrays.ts
@@ -0,0 +1,27 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [
+    ['f150'],
+    ['corolla'],
+    ['camaro']
+];
+// const carsByMake: string[][] = [];
+
+
+// Help with inference when extracting values
+const thisCar = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+    return car.toUpperCase()
+})
+
+// Flexible types
+const importantDates: (Date | string )[] = [];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
